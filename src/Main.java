8b568e9e7f2cb18fdb55529371d95b0d90e37e55
mdr20091 <rmatthew774@gmail.com>
2020-12-28 14:29:43 -0500

    test: getting the rust off

--- src/Main.java
+++ src/Main.java
@@ -5,9 +5,11 @@ public class Main {
         int lib  =100;
         if (lib > 99){
             System.out.println("you got the high score ");
-            lib = 0;
 
-            //hello world
+            if(lib > 99){
+                System.out.println("okay");
+
+            }
         }
 
 
